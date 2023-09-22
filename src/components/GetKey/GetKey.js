fefffdaeaab53da03ce606d7515be52560b843cc
Glenn Montera <glenn.montera@gmail.com>
2023-09-22 15:39:25 -0400

    fix warning messages

--- src/components/GetKey/GetKey.js
+++
@@ -1,6 +0,0 @@
-function GetKey() {
-  const key = "fa45cd22-e8f9-4794-950c-096f5585a8b7";
-  return key;
-}
-
-export default GetKey;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,6 @@ import Video from "../../components/Video/Video";
 import Description from "../../components/Description/Description";
 import CommentSection from "../../components/CommentSection/CommentSection";
 import NextVideos from "../../components/NextVideos/NextVideos";
-import GetKey from "../../components/GetKey/GetKey";
 import axios from "axios";
 
 function Home() {
@@ -21,19 +20,27 @@ function Home() {
   const comments = videoDetails.comments;
   // Holds all Videos
   const [videoList, setVideoList] = useState([]);
-  // Pulls API key from other function
-  const key = GetKey();
   //  If data has loaded, set to true
   const [isLoading, setLoading] = useState(true);
   //   console.log(currentVideo);
   //Load data from API
 
   // useEffect sets Loading to true and default video on Mount
+
   useEffect(() => {
+    const getAppData = async () => {
+      try {
+        const data = await axios.get(`http://localhost:8080/videos/`);
+        setVideoList(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
     setLoading(true);
     // console.log("mounting");
     const defaultVideo = "84e96018-4022-434e-80bf-000ce4cd12b8";
     setCurrentVideo(defaultVideo);
+    getAppData();
   }, []);
 
   useEffect(() => {
@@ -51,17 +58,7 @@ function Home() {
   }, [videoId, videoDetails.length]);
 
   useEffect(() => {
-    const getAppData = async () => {
-      try {
-        const data = await axios.get(
-          `http://localhost:8080/videos/`
-          // `https://project-2-api.herokuapp.com/videos?api_key=${key}`
-        );
-        setVideoList(data.data);
-      } catch (err) {
-        console.log(err);
-      }
-
+    const getVideo = async () => {
       try {
         const details = await axios.get(
           `http://localhost:8080/videos/${currentVideo}`
@@ -72,7 +69,8 @@ function Home() {
         console.log(err);
       }
     };
-    getAppData();
+
+    getVideo();
   }, [currentVideo]);
 
   if (!isLoading) {
@@ -90,11 +88,7 @@ function Home() {
               views={videoDetails.views}
               likes={videoDetails.likes}
             />
-            <CommentSection
-              currentVideo={currentVideo}
-              apiKey={key}
-              comments={comments}
-            />
+            <CommentSection currentVideo={currentVideo} comments={comments} />
           </div>
           {<NextVideos current={currentVideo} data={videoList} />}
         </div>
